5fbd7069a2ec41df1b0b7f8adaa328397bf38640
Guilherme Rojas <guilherme.rribeiro@pagar.me>
2022-11-08 20:08:19 -0300

    add 404 page

---
+++ src/pages/MissingPage.js
@@ -0,0 +1,23 @@
+export function MissingPage(){
+	return(
+		<div className="container">
+			<div className="center">
+				<h2> 404 </h2>
+
+				<div className="row">
+					<span>
+						The content you are looking for was not found.
+					</span>
+				</div>
+
+				<div className="row">
+			        <a href="/projeto-site-musica-react">
+			        	<span>
+			        		Click here to return to the main page
+			        	</span>
+			        </a>
+				</div>
+			</div>
+		</div>
+    )
+}
